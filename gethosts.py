a23e28f877b1b94fab6f1bf359b614d97cf54307
KenanSUN <kenansun0@gmail.com>
2015-01-23 22:36:22 +0800

    [2015/01/23] Now handle invalid url or hosts not found errors

--- gethosts.py
+++ gethosts.py
@@ -5,11 +5,20 @@ import urllib2
 import re
 
 def write_hosts(f):
-    url = "http://www.360kb.com/kb/2_122.html"
+    '''Try to get hosts file content from the Internet.
+returns -1 if url is no longer valid
+        -2 if no hosts found
+         0 otherwise'''
+    try:
+        url = "http://www.360kb.com/kb/2_122.html"
+    except:
+        return -1
     response = urllib2.urlopen(url)
     start = False
     while True:
         line = response.readline()
+        if line == '':
+            return -2
         if start == False and "#google" in line and "hosts" in line:
             start = True
             f.write("#google hosts begin\n")
@@ -21,16 +30,21 @@ def write_hosts(f):
             #print line 
         if start == True and "#google" in line and "hosts" in line:
             start = False
-            break
-    
+            return 0
 def main():
-    #f_name = "C:\Windows\System32\drivers\etc\hosts"
     f_name = "hosts"
     f = open(f_name,"w")
-    write_hosts(f)
+    res =  write_hosts(f)
     f.close()
-
-
+    return res
 if __name__ == "__main__":
-    main()
-
+    res = main()
+    if res != 0:
+        # We got an error here
+        # Should it send back an email to us ??
+        # We only handle the error by printing some info to the console
+        # TODO
+        if res == -1:
+            print "Invalid url!"
+        elif res == -2:
+            print "Hosts not found!"
